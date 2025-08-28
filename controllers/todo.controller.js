838f456715edd50df632459e20ce157472fcf9b7
M.A. Basar <basarbr7@gmail.com>
2025-08-28 16:32:51 +0600

    added  singletodo controller

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -33,7 +33,7 @@ const createTodo = async (req, res)=> {
     }    
 }
 
-const getTodoByUser = async (req, res)=>{
+const getAllTodoByUser = async (req, res)=>{
     try {
         const userId = req.params.id;
         const todos = await Todo.find({userId})
@@ -49,4 +49,19 @@ const getTodoByUser = async (req, res)=>{
     }
 }
 
-module.exports = { createTodo, getAllTodo, getTodoByUser }
+const getSingleTodo = async (req, res)=>{
+    try {
+        const { id } = req.params
+        const todo = await Todo.findById(id)
+        if (!todo) {
+            return res.status(404).json({ message: "Todo not found" });
+        }
+
+        res.status(200).json({ todo });
+    } catch (error) {
+        console.error('Error Fetching todo:', error.message)
+        res.status(500).json({ message: 'Internal server error' })
+    }
+}
+
+module.exports = { createTodo, getAllTodo, getAllTodoByUser, getSingleTodo }
--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,11 +1,12 @@
 const route = require('express').Router();
 
 // Importing the todo controller functions
-const { createTodo, getAllTodo, getTodoByUser } = require('../controllers/todo.controller')
+const { createTodo, getAllTodo, getAllTodoByUser, getSingleTodo } = require('../controllers/todo.controller')
 const auth = require('../middleware/verifyToken')
 
 route.get('/', getAllTodo)
 route.post('/', auth, createTodo)
-route.get('/user/:id', auth,  getTodoByUser)
+route.get('/user/:id', auth,  getAllTodoByUser)
+route.get('/:id', auth, getSingleTodo)
 
 module.exports = route
