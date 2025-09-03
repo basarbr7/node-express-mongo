a104fcd5e9a0d5f069f24e6d4b5d9a8ca2af0c4a
M.A. Basar <basarbr7@gmail.com>
2025-09-03 23:57:49 +0600

    added

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    // credentials: true,
 }));
 
 app.use(express.json());
